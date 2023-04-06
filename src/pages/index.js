9e92b8bec9fa52a882452cc081de45e88a2a39cc
VanshSharma14 <vansh.business14@gmail.com>
2023-04-06 17:51:39 -0400

    ye

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,16 +14,16 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <div className="grid h-screen w-screen flex">
-        <header className="grid m-1 h-10 w-full bg-black border-4 rounded-full place-self-start fixed border-transparent ease-in duration-100 hover:bg-white">
-          <p className="hover:text-green-700 text-green-400 font-mono align-middle pl-4 text-xl self-center ">
-            $git clone git:github@VanshSharma14
+        <header className="grid m-2 h-10 w-full bg-black border-4 rounded-full flex flex-row place-self-start fixed border-transparent ease-in duration-300 hover:bg-white">
+          <p className="ease-in duration-300 hover:text-green-700 text-green-400 font-mono align-middle pl-4 text-xl self-center ">
+            $ git clone git:github@VanshSharma14/resume-website.git
           </p>
         </header>
         <div className="container mx-auto flex flex-col place-items-center place-content-center">
-          <p className="tracking-tighter text-8xl font-bold ease-in duration-100 hover:text-9xl">
+          <h1 className="tracking-tighter text-8xl font-bold">
             Hi, I'm Vansh
-          </p>
-          <p className="text-2xl font-bold ease-in duration-100 hover:text-4xl">
+          </h1>
+          <p className="text-3xl font-bold">
             Software Engineer
           </p>
         </div>
