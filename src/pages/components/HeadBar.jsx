e0708c563a57aaab80aac8f7be9c5e883e62d22c
VanshSharma14 <goldenpotato14@gmail.com>
2023-07-25 22:17:57 -0400

    I hate myself

--- src/pages/components/HeadBar.jsx
+++ src/pages/components/HeadBar.jsx
@@ -1,13 +1,17 @@
 import GitHubLogo from "./logos/GitHubLogo";
+import LinkedInLogo from "./logos/LinkedInLogo";
 
 const HeadBar = () => {
     return (
-        <header className="my-2 w-screen h-10 bg-black rounded-full flex flex-row place-self-start 
+        <header className="my-2 w-screen h-10 bg-black rounded-full flex flex-row place-self-start
         fixed border-transparent">
-            <p className="w-fit text-green-400 font-mono mx-4 text-xl self-center ">
-                $ git clone git:github@VanshSharma14/resume-website.git
-            </p>
-            <GitHubLogo className="w-48" />
+            <div className="flex">
+                <p className="w-fit text-green-400 font-mono mx-4 text-xl self-center ">
+                    $ git clone git:github@VanshSharma14/resume-website.git
+                </p>
+                <GitHubLogo />
+                <LinkedInLogo />
+            </div>
         </header>
     );
 };
--- src/pages/components/logos/LinkedInLogo.jsx
+++ src/pages/components/logos/LinkedInLogo.jsx
@@ -1,11 +1,11 @@
-const GitHubLogo = () => {
+const LinkedInLogo = () => {
     return (
         <div className="place-self-center w-7 h-7">
             <a className="inline-flex" href="https://github.com/VanshSharma14" width="25" height="25">
-                <img className='invert inline-flex allign-bottom' width="25" height="25" src='/static/github.svg' alt="github logo" />
+                <img className='invert inline-flex allign-bottom' width="25" height="25" src='/static/linkedin.svg' alt="linkedin logo" />
             </a>
         </div>
     );
 };
 
-export default GitHubLogo;
+export default LinkedInLogo;
