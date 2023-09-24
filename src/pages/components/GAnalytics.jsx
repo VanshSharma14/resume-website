610d128bd7c68a5dbacc016928d00e821526d5da
VanshSharma14 <vansh.business14@gmail.com>
2023-09-24 15:05:22 -0400

    Added GoogleAnalytics Tag

---
+++ src/pages/components/GAnalytics.jsx
@@ -0,0 +1,24 @@
+import Script from 'next/script'
+
+const GAnalytics = () => {
+    return (
+
+        <div className="container">
+            <Script src="https://www.googletagmanager.com/gtag/js?id=G-VZV2D7WP5M" />
+            <Script id="google-analytics">
+                {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+  
+            gtag('config', 'G-VZV2D7WP5M');
+          `}
+            </Script>
+        </div>
+    );
+};
+
+export default GAnalytics;
+
+
+
