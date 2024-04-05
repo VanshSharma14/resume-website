2c56803d6ce9168699904d9560284dacb6753d35
VanshSharma14 <goldenpotato14@gmail.com>
2024-04-05 00:26:45 -0400

    literally only downloaded charkra and now i gotta go to bed. rigged ahh shi i tell you fukin bich ahh L ahh stupid ahh society man givin me a dentist apt when i just wanna charkra my ui type shi ykwis like cut me a damn break. now i gotta wake up early ah fuh for no fuking reason man fuhk

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,12 @@
 import '@/styles/globals.css'
 import '@/styles/hamburger.css'
+import { ChakraProvider } from "@chakra-ui/react"
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return(
+  <ChakraProvider> 
+    <Component {...pageProps} />
+  </ChakraProvider>
+ )
 }
