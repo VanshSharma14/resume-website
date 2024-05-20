a9c5ec2e5461719951a26f43863c1fe5f8c0d7f2
Vansh Sharma <68200448+VanshSharma14@users.noreply.github.com>
2024-05-20 12:36:47 -0400

    UI update2 (#7)
    * pls
    
    * pls2
    
    * FINALLY PLEASSEEE JUST SPARE ME ITS 247AM AHHH
    
    * scroll workin ayyyy
    
    * JAN LOOK HERE AHAHAH
    
    * updateski
    
    * FINAL UPDATE HOLY FUCKING SHIT SPARTAAA
    
    * added arrow and fixed missing info
    
    * final touches on overall layout, fixed other layout and scaling issues

--- src/pages/components/Title.jsx
+++ src/pages/components/Title.jsx
@@ -2,13 +2,13 @@ const Title = () => {
     return (
         <div className="container w-min md:mx-10 flex flex-col
             text-[#333533]">
-            <h1 className="tracking-tighter text-center md:text-left font-bold text-8xl">
+            <h1 className="tracking-tighter text-center md:text-left font-bold text-6xl md:text-8xl">
                 Hi,
                 <div>I&apos;m </div>
                 <div className="text-[#F5CB5C]">Vansh</div>
                 <div>Sharma</div>
             </h1>
-            <p className="text-3xl text-center md:text-left font-bold">
+            <p className="text-xl md:text-3xl text-center md:text-left font-bold">
                 Software Engineer
             </p>
         </div >
